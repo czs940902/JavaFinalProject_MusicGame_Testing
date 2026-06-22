5b14e22b00ae9b1557de4d28d3f24a4914684e6c
czs940902 <czs940902@icloud.com>
2026-06-22 19:54:32 +0800

    Remove reference files (commited by mistake)

--- reference/core/TimingEngine.java
+++
@@ -1,9 +0,0 @@
-package com.rhythmgame.core;
-
-import com.rhythmgame.audio.MusicPlayer;
-
-public class TimingEngine {
-    public static long getTimeMs() {
-        return MusicPlayer.getPositionMs();
-    }
-}
